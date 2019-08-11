8d162adb710b0e2d91495fc035d237661be35156
Marcuz.Huang <marcuz.huang@gmail.com>
2019-08-11 21:12:52 +0800

    average function

--- reviewanalytic/read.py
+++ reviewanalytic/read.py
@@ -4,4 +4,12 @@ with open('reviews.txt','r') as f:
 	for line in f:
 		data.append(line)
 		if len(data) % 1000 == 0 :
-		 print(len(data)/1000000)
+		 print(len(data))
+print('檔案讀取完畢, 總共有:',len(data),'筆資料')
+
+sum_len = 0
+
+for d in data:
+	sum_len = sum_len + len(d)
+print('字數一共有 :',sum_len)
+print('每筆留言平均字數 :',sum_len/1000000)
